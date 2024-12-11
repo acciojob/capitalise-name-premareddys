a2ea8ba40a4e11d7ecba3868c2b60d03a82581b3
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2024-12-11 15:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let div=document.getElementById('fname');
+div.addEventListener('mouseleave',function () {
+	let value=div.value.toUpperCase();
+	div.innerHtml=value;
+})
+
